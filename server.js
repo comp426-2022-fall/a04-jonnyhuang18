c3a7c26a6192f55d95db06fdf7ffd61d845c1f1a
Jonny Huang <jsuwen@ad.unc.edu>
2022-11-30 16:37:03 -0500

    200 OK

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const port = args.port||5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.listen(port);
 
 app.get("*", (req,res) => {
     return res.send("404 NOT FOUND");
+});
+
+app.get("/app/", (req,res) => {
+    return res.send("200 OK");
 });
