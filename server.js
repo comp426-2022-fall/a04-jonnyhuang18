7b0b64cfe2ffe51791c9175bf90adfeabe55e4c6
Jonny Huang <jsuwen@ad.unc.edu>
2022-11-30 16:33:13 -0500

    server

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 import { roll } from "./lib/roll.js";
 import express from "express";
 import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+const app = express();
+const port = args.port||5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("*", (req,res) => {
+    return res.send("404 NOT FOUND");
+});
