d697abf4a17f831e15a2da5003a4fd2e9a56fb80
Jonny Huang <jsuwen@ad.unc.edu>
2022-12-01 11:03:58 -0500

    stats

--- server.js
+++ server.js
@@ -48,3 +48,7 @@ app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
 app.get("*", (req,res) => {
     res.status(404).send("404 NOT FOUND");
 });
+
+app.get("*", (req,res) => {
+    res.status(404).send("404 NOT FOUND");
+});
